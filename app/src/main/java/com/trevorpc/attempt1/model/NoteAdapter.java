063b6934b635997586b909773b517f0e81be07a6
Trevor Prescott <prescotttj93@gmail.com>
2019-02-13 15:26:24 -0500

    initial commit

---
+++ app/src/main/java/com/trevorpc/attempt1/model/NoteAdapter.java
@@ -0,0 +1,56 @@
+package com.trevorpc.attempt1.model;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.trevorpc.attempt1.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    private List<Note> notes = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.note_item,viewGroup,false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int i) {
+        Note currentNote = notes.get(i);
+        holder.tvTitle.setText(currentNote.getTitle());
+        holder.tvDescription.setText(currentNote.getDescription());
+        holder.tvPriority.setText(String.valueOf(currentNote.getPriority()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView tvTitle, tvDescription, tvPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.text_view_title);
+            tvDescription = itemView.findViewById(R.id.text_view_description);
+            tvPriority = itemView.findViewById(R.id.text_view_priority);
+
+        }
+    }
+}
